b1f72d4e3d01b2bcc609cbaeee938abada348229
n2749 <nurbolat.dav@gmail.com>
2024-12-07 20:33:47 +0500

    refactoring: removed unnessesary print

--- src/config_reader.py
+++ src/config_reader.py
@@ -5,5 +5,4 @@ def get(section: str, field: str):
     config = configparser.ConfigParser()
     config_path = "config.ini"
     config.read(config_path)
-    print(config.sections())
     return config[section][field]
